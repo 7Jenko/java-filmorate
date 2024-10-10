b28456e345b72bb98e106d7659d1bf7d946a1074
Denis 7Jenko Semizhenko <wi-q@yandex.ru>
2024-10-11 02:29:35 +0300

    controllers-films-users Исправить failed assertions.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -5,9 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -54,7 +57,7 @@ public class FilmController {
     }
 
     @GetMapping
-    public Map<Integer, Film> getAllFilms() {
-        return films;
+    public List<Film> getAllUsers() {
+        return new ArrayList<>(films.values());
     }
 }
